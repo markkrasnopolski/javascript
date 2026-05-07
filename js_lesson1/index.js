347aa7c406542c612569ee25f444987b4a40f02a
markkrasnopolski <mark.krasnopolski@gmail.com>
2026-05-07 11:45:44 +0300

    Add files via upload

---
+++ js_lesson1/index.js
@@ -0,0 +1,71 @@
+// document.write("Hello JS!");
+// console.log("Hello World!");
+// console.info("Wassup World!");
+// console.error("Hello World!");
+// console.warn("Wassup World!");
+
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+//
+// var MySecondVar = '23';
+// console.log(MySecondVar);
+// console.log(MySecondVar - MyFirstVar);
+//
+// var int = 10;
+// var float = 12.5;
+// var bool = true; //false
+// var str = 6;
+// var a;
+// a = null
+// console.log(a);
+
+// let b = 10, a = b + 2;
+
+// var a;
+// a = 10;
+// console.log(a);
+
+// const PI = 3.14;
+
+// let a = 10, b = 20;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+
+// let a,b;
+//  a=2+4;
+//  b=2*(a= 10 + 3);
+// a= a+1;
+// a += 1;
+//
+// a++; //суфіксний інкремент
+// a--; //суфіксний декремент
+//
+// ++a;
+// --a;
+//
+// let a=2,b;
+//
+// b=++a;
+// console.log(b);
+
+// let a = 2, b;
+// b = a++;
+// console.log(b);
+// console.log(a);
+
+let a = '3', b = '5';
+console.log((a-0) + (b-0));
+console.log(Number(a) + Number(b));
+
+//Інтерполяція, конкатинація
+let name = 'mark', age = 5;
+console.log("Maboy" + name + " is " + age + " years old.");
+console.log(`Maboy ${name} is ${age} years old.`);
+
+let skill ="WEB";
+console.log(`Hello ${name}: your best skill is - ${skill} ${2*4}`);
